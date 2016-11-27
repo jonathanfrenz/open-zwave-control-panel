fc0f20ec8b1f32be8189930080afca1a03c85c67
jonathanfrenz <jonathanfrenz@gmail.com>
2016-11-26 19:04:15 -0600

    asdf

--- temperature_conversion.cpp
+++ temperature_conversion.cpp
@@ -1,9 +1,14 @@
 #include <cstdio>
 #include <cstring>
+#include <string>
+#include <sstream>
+#include <vector>
 
 #include "temperature_conversion.h"
 #include <mosquittopp.h>
 #include <fstream>
+
+extern void onIncomingMessage(string conn_arg1, string arg);
 
 mqtt_tempconv::mqtt_tempconv() : mosquittopp("dumb id")
 {
@@ -38,15 +43,46 @@ void mqtt_tempconv::on_connect(int rc)
 	printf("Connected with code %d.\n", rc);
 	if(rc == 0){
 		/* Only attempt to subscribe on a successful connect. */
-		subscribe(NULL, "iot/test");
+		subscribe(NULL, "iot/down");
 	}
+}
+
+void split(const std::string &s, char delim, std::vector<std::string> &elems) {
+    std::stringstream ss;
+    ss.str(s);
+    std::string item;
+    while (std::getline(ss, item, delim)) {
+        elems.push_back(item);
+    }
+}
+
+
+std::vector<std::string> split(const std::string &s, char delim) {
+    std::vector<std::string> elems;
+    split(s, delim, elems);
+    return elems;
 }
 
 void mqtt_tempconv::on_message(const struct mosquitto_message *message)
 {
-	double temp_celsius, temp_farenheit;
 	char buf[51];
 	printf("Got: %s\n", message->payload);
+	
+	if(!strcmp(message->topic, "iot/down")){
+		memset(buf, 0, 51*sizeof(char));
+		
+		memcpy(buf, message->payload, 50*sizeof(char));
+		printf("Got: %s\n", buf);
+		
+		std::vector<std::string> splits = split(string(buf),  '\n');
+		if (splits.size == 2)
+		{
+			onIncomingMessage(splits[0], splits[1]);
+		}
+		//temp_celsius = atof(buf);
+		//temp_farenheit = temp_celsius*9.0/5.0 + 32.0;
+		//publish(NULL, "temperature/farenheit", strlen(buf), buf);
+	}
 /*
 	if(!strcmp(message->topic, "test/cool")){
 		memset(buf, 0, 51*sizeof(char));
